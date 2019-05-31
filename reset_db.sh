bffd334c4a1c98e54659ba1b17ff602201cd4c9e
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2019-05-31 09:23:47 +0200

    add authentication app

---
+++ reset_db.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+rm -f db.sqlite3
+
+./manage.py migrate --noinput --verbosity=1
+
+./manage.py loaddata \
+    auth_admin
--- settings/common.py
+++ settings/common.py
@@ -1,1 +1,12 @@
 from .default import *
+
+
+INSTALLED_APPS = [
+    'authentication',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
